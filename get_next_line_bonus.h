d4f6d617aceabe2eb4bd4dc4c8718b56c9fa2c0e
Salahdine Ouhsain <souhsain@e1r2p4.1337.ma>
2025-11-21 21:03:58 +0100

    add

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -6,17 +6,17 @@
 /*   By: souhsain <souhsain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/06 21:31:52 by souhsain          #+#    #+#             */
-/*   Updated: 2025/11/21 20:43:31 by souhsain         ###   ########.fr       */
+/*   Updated: 2025/11/21 21:01:36 by souhsain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_BONUS_H
 # define GET_NEXT_LINE_BONUS_H
 
-# define OPEN_MAX 1024
+#define OPEN_MAX 1024
 
 # ifndef SIZE_MAX
-#  define SIZE_MAX 18446744073709551615
+#  define SIZE_MAX 18446744073709551615ULL
 # endif
 
 # ifndef BUFFER_SIZE
